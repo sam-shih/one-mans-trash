a97e471f9cb2f779cedf256347b79ed7a3888dcb
zackcarlson <zackt.carlson@gmail.com>
2018-05-31 15:36:42 -0700

    Update gitignore / add user handler function

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,10 +1,27 @@
-var express = require('express');
-var session = require('express-session');
-var bodyParser = require('body-parser');
+const db = require('../../database/users-model');
+const session = require('express-session');
+const bcrypt = require('bcrypt');
+const bodyParser = require('body-parser');
 
-var app = express();
-app.use(bodyParser.json());
+module.exports = {
+  get: function() {
 
+  },
+  post: function() {
+    db.User.pre('save', function(next) {
+      let user = this;
+      if (!user.isModified('password')) { return next(); }
+      bcrypt.genSalt(10, function(err, salt) {
+        if (err) { return next(err); }
+        bcrypt.hash(user.password, salt, function(err, hash) {
+          if (err) { return next(err); }
+          user.password = hash;
+          next();
+        })
+      });
+    })
+  }
+}
 //_.dirname?
 // becrypt.compare
 // becrypt.has
