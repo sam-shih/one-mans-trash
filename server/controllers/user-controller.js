c18620b3b9ba5e028b97541c496d95317e14b817
Don de Rojas <derojasdonny@gmail.com>
2018-05-30 22:37:30 -0700

    installed bcrypt and express sessions

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,2 +1,11 @@
 var express = require('express');
+var session = require('express-session');
 var bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.json());
+
+//_.dirname?
+// becrypt.compare
+// becrypt.has
+//
